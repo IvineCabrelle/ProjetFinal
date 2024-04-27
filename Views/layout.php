4af1ad7cb959915ae6c1b0bf11d51469bdb2b4dc
IvineCabrelle <144458237+IvineCabrelle@users.noreply.github.com>
2024-04-26 20:21:12 -0400

    deconnections

---
+++ Views/layout.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Déconnexion</title>
+    <link rel="stylesheet" href="../Style/styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Déconnexion réussie !</h1>
+        <p>Merci d'avoir utilisé notre site. Vous êtes maintenant déconnecté.</p>
+        <p class="login-message">Vous n'avez pas de compte ? <a href="../Views/VueInscription.php">Inscrivez-vous</a></p>
+    </div>
+</body>
+</html>
